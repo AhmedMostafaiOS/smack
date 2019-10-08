445adde9cd7edfad95741b6d7750a0102ddee4c2
Ahmed Mostafa <ahmedmostafa@Ahmeds-MacBook-Air.local>
2019-10-08 19:11:33 +0200

    smack app is slack clone

---
+++ build/Pods.build/Debug-iphonesimulator/Alamofire.build/Objects-normal/x86_64/Alamofire-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/ahmedmostafa/Desktop/{11}\ -\ Slack\ App\ Creating\ a\ Slack\ App\ \ Working\ With\ APIs/{104}\ -\ I'm\ Back!\ [7.43]/team-devslopes-app-smack-0a6ffbb00d86/Pods/Target\ Support\ Files/Alamofire/Alamofire-dummy.m \
+  /Users/ahmedmostafa/Desktop/{11}\ -\ Slack\ App\ Creating\ a\ Slack\ App\ \ Working\ With\ APIs/{104}\ -\ I'm\ Back!\ [7.43]/team-devslopes-app-smack-0a6ffbb00d86/build/Pods.build/Debug-iphonesimulator/Alamofire.build/module.modulemap \
+  /Users/ahmedmostafa/Desktop/{11}\ -\ Slack\ App\ Creating\ a\ Slack\ App\ \ Working\ With\ APIs/{104}\ -\ I'm\ Back!\ [7.43]/team-devslopes-app-smack-0a6ffbb00d86/Pods/Target\ Support\ Files/Alamofire/Alamofire-prefix.pch
---
+++ build/Pods.build/Debug-iphonesimulator/Alamofire.build/Objects-normal/x86_64/Alamofire_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ahmedmostafa/Desktop/{11}\ -\ Slack\ App\ Creating\ a\ Slack\ App\ \ Working\ With\ APIs/{104}\ -\ I'm\ Back!\ [7.43]/team-devslopes-app-smack-0a6ffbb00d86/build/Pods.build/Debug-iphonesimulator/Alamofire.build/DerivedSources/Alamofire_vers.c
